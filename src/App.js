1488268b95db1bf67bff15a2a358ddba69481905
sabrina-jones <sabrina.jones87@gmail.com>
2021-06-28 18:08:03 -0500

    commit tree fa521cffbee5b7483ead5ed6cc52f4424bc06761

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+  
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
